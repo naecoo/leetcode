897a663090b8276d07c1a2eb222716cd15ad6d46
ocean <ocean7hy@gmail.com>
2022-05-30 10:41:00 +0800

    2022/05/30

--- 2022-05-30/sum-of-root-to-leaf-binary-numbers.js
+++ 2022-05-30/sum-of-root-to-leaf-binary-numbers.js
@@ -14,6 +14,43 @@
  * @return {number}
  */
 function sumRootToLeaf(root) {
+  const dfs = (node, val) => {
+    if (!node) return 0;
+    val = (val << 1) | node.val;
+    if (!node.left && !node.right) return val;
+    return dfs(node.left, val) + dfs(node.right, val);
+  }
+  return dfs(root, 0);
+};
+
+// 迭代
+function sumRootToLeaf1(root) {
+  const stack = [];
+  let val = 0, ret = 0;
+  let prev = null;
+  while (root || stack.length) {
+    while (root) {
+      val = (val << 1) | root.val;
+      stack.push(root);
+      root = root.left;
+    }
+    root = stack[stack.length - 1];
+    if (!root.right || root.right === prev) {
+      if (!root.left && !root.right) {
+        ret += val;
+      }
+      val >>= 1;
+      stack.pop();
+      prev = root;
+      root = null;
+    } else {
+      root = root.right;
+    }
+  }
+  return ret;
+}
+
+function sumRootToLeaf2(root) {
   let sum = 0;
   const paths = [];
   backtrack(root);
