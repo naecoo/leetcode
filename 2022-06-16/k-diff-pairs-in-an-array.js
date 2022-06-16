3b0285d2eb17f915a62c9edbb0d1a5f87b004149
ocean <ocean7hy@gmail.com>
2022-06-16 22:47:01 +0800

    2022/06/16

--- 2022-06-16/k-diff-pairs-in-an-array.js
+++ 2022-06-16/k-diff-pairs-in-an-array.js
@@ -10,6 +10,7 @@ function findPairs(nums, k) {
   const visited = new Set();
   const res = new Set();
   nums.forEach(num => {
+    // 因为(0,1) 和（1,0）是相同的一对，这里我们存储小的数字就可以了
     if (visited.has(num + k)) {
       res.add(num);
     }
