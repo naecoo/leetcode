4301ab016a733feb428d5ff6d799b979a9d6c15c
ocean <ocean7hy@gmail.com>
2022-06-30 09:16:03 +0800

    2022/06/30

---
+++ 2022-06-30/prime-arrangements.js
@@ -0,0 +1,39 @@
+/**
+ * 题目链接：
+ * https://leetcode.cn/problems/prime-arrangements/
+ *
+ */
+const MOD = 1000000007;
+var numPrimeArrangements = function (n) {
+  let numPrimes = 0;
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i)) {
+      numPrimes++;
+    }
+  }
+  let res = 1;
+  let m = n - numPrimes;
+  while (numPrimes > 0) {
+    res = res % MOD;
+    res *= numPrimes;
+    numPrimes--;
+  }
+  while (m > 0) {
+    res = res % MOD;
+    res *= m;
+    m--;
+  }
+  return res;
+};
+
+const isPrime = (n) => {
+  if (n === 1) {
+    return false;
+  }
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
