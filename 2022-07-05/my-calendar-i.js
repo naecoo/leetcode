89a3beefca2029afccaebf3db927243fb88bbf4d
ocean <ocean7hy@gmail.com>
2022-07-05 23:56:23 +0800

    2022/07/05

--- 2022-07-05/my-calendar-i.js
+++ 2022-07-05/my-calendar-i.js
@@ -3,3 +3,17 @@
  * https://leetcode.cn/problems/my-calendar-i/
  *
  */
+var MyCalendar = function () {
+  this.booked = [];
+};
+
+MyCalendar.prototype.book = function (start, end) {
+  for (const arr of this.booked) {
+    let l = arr[0], r = arr[1];
+    if (l < end && start < r) {
+      return false;
+    }
+  }
+  this.booked.push([start, end]);
+  return true;
+};
