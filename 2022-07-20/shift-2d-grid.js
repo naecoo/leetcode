ab1a5aefa57d678c0d59bda1cd9b3312ea02dfe1
ocean <ocean7hy@gmail.com>
2022-07-20 22:48:22 +0800

    2022/07/20

---
+++ 2022-07-20/shift-2d-grid.js
@@ -0,0 +1,26 @@
+/**
+ * 题目链接：
+ * https://leetcode.cn/problems/shift-2d-grid/
+ *
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number[][]}
+ */
+function shiftGrid(grid, k) {
+  const m = grid.length, n = grid[0].length;
+  const ret = [];
+  for (let i = 0; i < m; i++) {
+    const row = [];
+    for (let j = 0; j < n; j++) {
+      row.push(0);
+    }
+    ret.push(row);
+  }
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      const index1 = (i * n + j + k) % (m * n);
+      ret[Math.floor(index1 / n)].splice(index1 % n, 1, grid[i][j]);
+    }
+  }
+  return ret;
+};
