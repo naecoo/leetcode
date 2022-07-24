783b041bb3bdd90f890dfbe2ae0e1848bedbebfb
ocean <ocean7hy@gmail.com>
2022-07-24 10:11:33 +0800

    2022/07/24

---
+++ 2022-07-24/distance-between-bus-stops.js
@@ -0,0 +1,27 @@
+/**
+ * 题目链接：
+ * https://leetcode.cn/problems/distance-between-bus-stops/
+ *
+ * @param {number[]} distance
+ * @param {number} start
+ * @param {number} destination
+ * @return {number}
+ */
+function distanceBetweenBusStops(distance, start, destination) {
+  let cost1 = 0;
+  let cost2 = 0;
+
+  if (start > destination) {
+    [start, destination] = [destination, start];
+  }
+
+  for (let i = start; i < destination; i++) {
+    cost1 += distance[i];
+  }
+
+  for (let i = destination; i !== start; i = (i + 1) % distance.length) {
+    cost2 += distance[i];
+  }
+
+  return Math.min(cost1, cost2);
+};
