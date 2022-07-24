384678de8b8dd8ddc2744b44711564edd023c17e
ocean <ocean7hy@gmail.com>
2022-07-24 10:12:43 +0800

    2022/07/24

--- 2022-07-24/distance-between-bus-stops.js
+++ 2022-07-24/distance-between-bus-stops.js
@@ -10,6 +10,18 @@
 function distanceBetweenBusStops(distance, start, destination) {
   let cost1 = 0;
   let cost2 = 0;
+  for (let i = start; i !== destination; i = (i + 1) % distance.length) {
+    cost1 += distance[i];
+  }
+  for (let i = destination; i !== start; i = (i + 1) % distance.length) {
+    cost2 += distance[i];
+  }
+  return Math.min(cost1, cost2);
+};
+
+function distanceBetweenBusStops2(distance, start, destination) {
+  let cost1 = 0;
+  let cost2 = 0;
 
   if (start > destination) {
     [start, destination] = [destination, start];
