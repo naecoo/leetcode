1ec908fedff57f9856744e32245cb01f86864e6a
ocean <ocean7hy@gmail.com>
2022-08-19 20:02:53 +0800

    2022/08/19

---
+++ 2022-08-19/number-of-students-doing-homework-at-a-given-time.js
@@ -0,0 +1,18 @@
+/**
+ * 题目链接：
+ * https://leetcode.cn/problems/number-of-students-doing-homework-at-a-given-time/
+ *
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number} queryTime
+ * @return {number}
+ */
+var busyStudent = function (startTime, endTime, queryTime) {
+  let count = 0;
+  for (let i = 0; i < startTime.length; i++) {
+    if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+      count++;
+    }
+  }
+  return count;
+};
